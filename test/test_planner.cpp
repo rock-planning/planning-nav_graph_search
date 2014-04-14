acf31b551d5274ce705cdbf724dc520049dc1fd4
Janosch Machowinski <janosch.machowinski@dfki.de>
2014-04-14 11:46:11 +0200

    fixed test cases

--- test/test_planner.cpp
+++ test/test_planner.cpp
@@ -8,60 +8,60 @@
 
 #include <sstream>
 
-#include <nav_graph_search/traversability_map.hpp>
+#include <envire/maps/TraversabilityGrid.hpp>
 #include <nav_graph_search/dstar_lite.hpp>
+#include <envire/Core.hpp>
 
 using namespace nav_graph_search;
 using namespace envire;
 
-size_t width = 500;
-size_t height = 500;
+size_t sizeX = 500;
+size_t sizeY = 500;
 int num_change_goal = 4;
 int num_change_start = 10;
-// Fill with class 0, size of each cell / pixel is set to 0.05m (smaller grids means reduced costs).
-TraversabilityMap trav_map(width, height, 0, 0.05); // Exploring a 25x25m area.
-TerrainClasses terrain_classes;
+int numTerrainClasses = 10;
+
+envire::Environment envfoo;
 
 // Using random values for each cell, which is suboptimal for planning.
 BOOST_AUTO_TEST_CASE( create_traversibility_map )
 {
     // Add classes (0 to 16) to the map.
     srand (time(NULL));
-    int class_value;
-    for(unsigned int y = 0; y < height; ++y) {
-        for(unsigned int x = 0; x < width; ++x) {
-            class_value = rand() % TraversabilityMap::CLASSES_COUNT; // 0 to 15
-            if(x == 0 || x == width-1 || y == 0 || y == height-1) {
+    uint8_t class_value;
+    
+    envire::TraversabilityGrid *trGrid = new envire::TraversabilityGrid(sizeY, sizeY, 0.05, 0.05);
+    envire::TraversabilityGrid::ArrayType &trData(trGrid->getGridData(TraversabilityGrid::TRAVERSABILITY));
+    trData.resize(boost::extents[sizeY][sizeX]);
+    
+    for(unsigned int y = 0; y < sizeY; ++y) {
+        for(unsigned int x = 0; x < sizeX; ++x) {
+            class_value = rand() % numTerrainClasses; // 0 to 15
+            if(x == 0 || x == sizeX-1 || y == 0 || y == sizeY-1) {
               class_value = 0; // Represents an obstacle.
             }
-            trav_map.setValue(x, y, class_value);
+            
+            trData[y][x] = class_value;
         }
     }
-}
-
-BOOST_AUTO_TEST_CASE( create_terrain_classes )
-{
-    terrain_classes.empty();
-    struct TerrainClass terrain_class;
-    for(int i=0; i<TraversabilityMap::CLASSES_COUNT; ++i) {
-      terrain_class.in = terrain_class.out = i;
-      terrain_class.cost = i; // Represents speed in m/sec
-      terrain_class.margin = 0;
-      std::stringstream ss;
-      ss << i;
-      terrain_class.name = ss.str();
-      terrain_classes.push_back(terrain_class);
+    
+    for(int i=0; i< numTerrainClasses; ++i) {
+        TraversabilityClass klass(1 - (1.0 / numTerrainClasses * i));
+        trGrid->setTraversabilityClass(i, klass);
     }
+
+    envfoo.attachItem(trGrid);
+    
 }
 
 /** Changes the traversability classes of 'width' cells. */
 void randUpdateMap(DStarLite& dstar_lite) {
     int cell_x = 0, cell_y = 0;
     int new_class = 0;
-    for(unsigned int i=0; i<width; i++) {
-        cell_x = rand()%width;
-        cell_y = rand()%height;
-        new_class = rand()%TraversabilityMap::CLASSES_COUNT;
+    for(unsigned int i=0; i<sizeX; i++) {
+        cell_x = rand()%sizeX;
+        cell_y = rand()%sizeY;
+        new_class = rand()%numTerrainClasses;
         dstar_lite.updateTraversability(cell_x, cell_y, new_class);
     }
 };
@@ -71,14 +71,14 @@ timeval start, end;
 void executePlanning(DStarLite& dstar_lite, std::string test_description) {
     double path_cost = 0;
     int start_x, start_y, goal_x, goal_y;
-    printf("%d map updates will be executed after each new start position\n", (int)width);
+    printf("%d map updates will be executed after each new start position\n", (int)sizeX);
     gettimeofday(&start, 0);    
     for(int i_goal=0; i_goal<num_change_goal; ++i_goal) {
-        goal_x = rand()%width;
-        goal_y = rand()%height;
+        goal_x = rand()%sizeX;
+        goal_y = rand()%sizeY;
         for(int i_start=0; i_start<num_change_start; ++i_start) {   
-            start_x = rand()%width;
-            start_y = rand()%height;
+            start_x = rand()%sizeX;
+            start_y = rand()%sizeY;
             randUpdateMap(dstar_lite);
             BOOST_REQUIRE_NO_THROW(path_cost = dstar_lite.run(goal_x, goal_y, start_x, start_y));
             printf("Path from start (%d,%d) to goal (%d,%d) cost: %4.2f sec.\n", start_x, start_y, goal_x, goal_y, path_cost);  
@@ -95,21 +95,21 @@ BOOST_AUTO_TEST_CASE( dstar_lite_planner_norobotsize_noinflatemax )
 {
     // IOnly single cells are updated (not affecting the surrounding cells).
     
-    DStarLite dstar_lite(terrain_classes);
+    DStarLite dstar_lite;
     executePlanning(dstar_lite, "norobotsize_noinflatemax");   
 }
 
 BOOST_AUTO_TEST_CASE( dstar_lite_planner_robotsize_noinflatemax )
 {
     // Uses the average cost within the footprint-/robot-size for updates.
-    DStarLite dstar_lite(terrain_classes);
+    DStarLite dstar_lite;
     executePlanning(dstar_lite, "robotsize_noinflatemax");
 }
 
 BOOST_AUTO_TEST_CASE( dstar_lite_planner_robotsize_inflatemax )
 {
     // Uses the maximal cost within the footprint-/robot-size for updates.
-    DStarLite dstar_lite(terrain_classes);
+    DStarLite dstar_lite;
     executePlanning(dstar_lite, "robotsize_inflatemax");
 }
 
